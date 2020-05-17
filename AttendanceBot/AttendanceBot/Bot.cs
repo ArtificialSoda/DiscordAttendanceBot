ee6ffbfc4b135ede87584c9c6e5c4eb08722e1e4
brentpereira <60619374+brentpereira@users.noreply.github.com>
2020-05-16 20:30:33 -0400

    Setup bot

---
+++ AttendanceBot/AttendanceBot/Bot.cs
@@ -0,0 +1,59 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AttendanceBot
+{
+    class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public InteractivityExtension Interactivity { get; private set; }
+        public CommandsNextExtension Commands { get; private set; } // Allows for commands to be set in the bot
+
+        public async Task RunAsync()
+        {
+            // load in the json config
+            var json = string.Empty; 
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration()
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true, // Reconnect automatically if the bot turns off
+                LogLevel = LogLevel.Debug, // Get all logs rather than just errors
+                UseInternalLogHandler = true
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.UseInteractivity(new InteractivityConfiguration{});
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix }, // Prefix used to communicate with bot
+                EnableDms = false, // Can only use commands on the server 
+                EnableMentionPrefix = true // Allows bot to be communicated with by mentioning it
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig); // Automatically handles commands
+
+            await Client.ConnectAsync(); // Connects the bot
+
+            await Task.Delay(-1); // Stops the bot from quitting early 
+        }
+    }
+}
---
+++ AttendanceBot/AttendanceBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AttendanceBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}

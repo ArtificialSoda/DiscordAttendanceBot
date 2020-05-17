fb637cb5369c7fdf09c0ab036273d3cf91d54bbc
brentpereira <60619374+brentpereira@users.noreply.github.com>
2020-05-16 21:08:59 -0400

    Added Comments
    Comments added to Bot and Attendance classes

--- AttendanceBot/AttendanceBot/Attendance.cs
+++ AttendanceBot/AttendanceBot/Attendance.cs
@@ -17,41 +17,42 @@ namespace AttendanceBot
         private List<string> _names = new List<string>();
 
         [Command("attendance")]
-        public async Task Poll(CommandContext ctx)
+        public async Task Poll(CommandContext ctx) // Takes in all information about the command
         {
-            TimeSpan duration = new TimeSpan(0, 0, 5);
+            TimeSpan duration = new TimeSpan(0, 0, 10); // How long the poll remains active for
 
             var interactivity = ctx.Client.GetInteractivity();
 
             var pollEmbed = new DiscordEmbedBuilder
             {
-                Title = "Attendance Poll",
+                Title = "Attendance Poll"
             };
 
             var pollMessage = await ctx.Channel.SendMessageAsync(embed: pollEmbed).ConfigureAwait(false);
 
-            await pollMessage.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":raised_hand:")).ConfigureAwait(false);
+            await pollMessage.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":raised_hand:")).ConfigureAwait(false); // Defines the emojis to be used in the poll
 
-            var result = await interactivity.CollectReactionsAsync(pollMessage, duration).ConfigureAwait(false);
+            var result = await interactivity.CollectReactionsAsync(pollMessage, duration).ConfigureAwait(false); // Collect poll reactions
 
             var results = result.Select(x => $"{x.Users.ToArray()[0]}");
 
-            string attendanceInfo = string.Join("\n", results);
+            string attendanceInfo = string.Join("\n", results); // Makes a list of the info of the students who answered the poll
 
-            string[] studentInfo = attendanceInfo.Split("\n");
+            string[] studentInfo = attendanceInfo.Split("\n"); // Seperates the list into individual lines and stores them
 
             string[] studentInfoSplit;
 
+            // Extracts just the username of each present student
             foreach (string item in studentInfo)
             {
                 studentInfoSplit = item.Substring(27).Split("#");
-                _names.Add(studentInfoSplit[0]);
+                _names.Add(studentInfoSplit[0]); // Stores each username in the List<> of names
             }
+        }
 
-            foreach (string item in _names)
-            {
-                Console.WriteLine(item);
-            }
+        public List<string> Names
+        {
+            get { return _names; }
         }
     }
 }
